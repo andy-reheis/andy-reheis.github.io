5a90b4ff981ce16cdc758443cd1a4bff9b921f60
andy-reheis <andreas.reheis@gmail.com>
2020-09-03 20:46:29 +0000

    Deploying to gh-pages from  @ d3854ee99ac69b4d74841db1096dd174c81b534c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72336dad82b8811fe9d53101469fcdbe.js"
+  "/precache-manifest.d571d8b374bf0221fb7ae8c11a1493a2.js"
 );
 
 self.addEventListener('message', (event) => {
